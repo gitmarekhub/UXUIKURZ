a327f7b1be7dd085deb567c6a937d98ecfd2efa3
jakubjirimarek <jakubjirimarek@gmail.com>
2025-12-22 17:43:46 +0100

    edit

--- assets/tour/data.js
+++ assets/tour/data.js
@@ -389,7 +389,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
